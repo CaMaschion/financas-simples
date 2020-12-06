2885781a2b53dada9c124e1ce28fc580d3573053
camila-maschion <camila.maschion@raizen.com>
2020-12-06 10:06:00 -0300

    authentication done

---
+++ app/src/main/java/com/financas_simples_android/model/request/AuthRequest.kt
@@ -0,0 +1,8 @@
+package com.financas_simples_android.model.request
+
+data class AuthRequest (
+
+    var email: String,
+    var password: String
+
+)
--- app/src/main/java/com/financas_simples_android/service/AuthService.kt
+++ app/src/main/java/com/financas_simples_android/service/AuthService.kt
@@ -1,13 +1,15 @@
 package com.financas_simples_android.service
 
+import com.financas_simples_android.model.request.AuthRequest
 import com.financas_simples_android.model.response.TokenResponse
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
 
     @POST("/v1/auth")
-    fun authentication() : Call<TokenResponse>
+    fun authentication(@Body body: AuthRequest) : Call<TokenResponse>
 }
 
 
