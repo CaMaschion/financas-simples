35858d186a64f68e08104de2b00283afa12dd3fb
camila <camila@camila-Aspire-A315-42G>
2021-01-24 16:13:48 -0300

    fix/recycler view

--- app/src/main/java/com/financas_simples_android/adapter/MovementsAdapter.kt
+++ app/src/main/java/com/financas_simples_android/adapter/MovementsAdapter.kt
@@ -8,15 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.financas_simples_android.R
 import com.financas_simples_android.model.response.MovementResponse
 
-class MovementsAdapter (private val movement: List<MovementResponse>): RecyclerView.Adapter<MovementsAdapter.ViewHolder>()  {
+class MovementsAdapter (private val movement: List<MovementResponse>):
+    RecyclerView.Adapter<MovementsAdapter.ViewHolder>()  {
 
         class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             val food: TextView = view.findViewById(R.id.comida)
         }
 
         override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-
-            val teste = movement
 
             val view = LayoutInflater.from(viewGroup.context)
                 .inflate(R.layout.list_movement, viewGroup, false)
