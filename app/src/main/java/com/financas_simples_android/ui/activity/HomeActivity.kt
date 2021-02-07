91ef88064aed3dc193c3dbfa1509f35dc38220e2
Bruno Almeida <brunosalmeida@outlook.com>
2021-02-07 18:44:40 +0000

    chore: add hamburguer menu

--- app/src/main/java/com/financas_simples_android/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/financas_simples_android/ui/activity/HomeActivity.kt
@@ -1,8 +1,13 @@
 package com.financas_simples_android.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.financas_simples_android.R
@@ -10,26 +15,92 @@ import com.financas_simples_android.adapter.MovementsAdapter
 import com.financas_simples_android.model.response.MovementResponse
 import com.financas_simples_android.service.ApiService
 import com.financas_simples_android.service.MovementService
+import com.google.android.material.navigation.NavigationView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.content.res.Configuration as Configuration1
+
 
 class HomeActivity : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
     lateinit var movementAdapter: MovementsAdapter
+    private var mDrawer: DrawerLayout? = null
+    private var toolbar: Toolbar? = null
+    private val nvDrawer: NavigationView? = null
 
+    private var drawerToggle: ActionBarDrawerToggle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // This will display an Up icon (<-), we will replace it with hamburger later
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+
+        // Find our drawer view
+        mDrawer = findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
+
+        // Setup toggle to display hamburger icon with nice animation
+        drawerToggle!!.isDrawerIndicatorEnabled = true;
+        drawerToggle!!.syncState();
+        mDrawer!!.addDrawerListener(drawerToggle!!);
 
         recyclerView = findViewById(R.id.rv_last_movement)
 
         getMovements()
     }
 
-    fun getMovements() {
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle!!.syncState()
+    }
+
+
+    override fun onConfigurationChanged(newConfig: Configuration1) {
+        super.onConfigurationChanged(newConfig)
+        // Pass any configuration change to the drawer toggles
+        drawerToggle!!.onConfigurationChanged(newConfig)
+    }
+
+    @JvmName("onOptionsItemSelected1")
+    fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return if (drawerToggle!!.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item!!)
+    }
+
+    private fun setupDrawerToggle(): ActionBarDrawerToggle {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return ActionBarDrawerToggle(
+            this,
+            mDrawer,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // The action bar home/up action should open or close the drawer.
+        when (item.itemId) {
+            android.R.id.home -> {
+                mDrawer!!.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getMovements() {
 
         val request = ApiService.buildService(MovementService::class.java)
         val call = request.getMovements()
@@ -39,9 +110,9 @@ class HomeActivity : AppCompatActivity() {
                 call: Call<List<MovementResponse>>,
                 response: Response<List<MovementResponse>>
             ) {
-                if(response.isSuccessful){
+                if (response.isSuccessful) {
 
-                    var body = response.body()
+                    val body = response.body()
                     movementAdapter = body?.let { MovementsAdapter(it) }!!
 
                     recyclerView.apply {
--- app/src/main/java/com/financas_simples_android/ui/activity/MenuActivity.kt
+++ app/src/main/java/com/financas_simples_android/ui/activity/MenuActivity.kt
@@ -1,62 +1,62 @@
-package com.financas_simples_android.ui.activity
-
-import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import com.financas_simples_android.R
-
-class MenuActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    lateinit var drawerLayout: DrawerLayoutrawerLayout
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
-
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
-
-
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_new_movement_fragment, R.id.nav_my_investment_fragment, R.id.nav_my_movement_fragment
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-}
+//package com.financas_simples_android.ui.activity
+//
+//import android.os.Bundle
+//import android.view.Menu
+//import com.google.android.material.floatingactionbutton.FloatingActionButton
+//import com.google.android.material.snackbar.Snackbar
+//import com.google.android.material.navigation.NavigationView
+//import androidx.navigation.findNavController
+//import androidx.navigation.ui.AppBarConfiguration
+//import androidx.navigation.ui.navigateUp
+//import androidx.navigation.ui.setupActionBarWithNavController
+//import androidx.navigation.ui.setupWithNavController
+//import androidx.drawerlayout.widget.DrawerLayout
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.appcompat.widget.Toolbar
+//import com.financas_simples_android.R
+//
+//class MenuActivity : AppCompatActivity() {
+//
+//    private lateinit var appBarConfiguration: AppBarConfiguration
+//    lateinit var drawerLayout: DrawerLayoutrawerLayout
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_menu)
+//
+//        val toolbar: Toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+//        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+//
+//
+//        val fab: FloatingActionButton = findViewById(R.id.fab)
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+//
+//        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+//        val navView: NavigationView = findViewById(R.id.nav_view)
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.nav_new_movement_fragment, R.id.nav_my_investment_fragment, R.id.nav_my_movement_fragment
+//            ), drawerLayout
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+//    }
+//
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.menu, menu)
+//        return true
+//    }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//    }
+//}
